64b3d631d78604b9466e4b19589de5b9f90ff9d0
DongXin <991189873@qq.com>
2020-04-02 21:52:22 +0800

    CRC 添加 2020年4月2日21:52:14

--- app/src/main/java/com/usr/usrsimplebleassistent/Utils/CRC.java
+++ app/src/main/java/com/usr/usrsimplebleassistent/Utils/CRC.java
@@ -180,4 +180,20 @@ public class CRC {
         return i;
     }
 
+    /**
+     *      * 16进制表示的字符串转换为字节数组
+     *      *
+     *      * @param s 16进制表示的字符串
+     *      * @return byte[] 字节数组
+     *      
+     */
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] b = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            // 两位一组，表示一个字节,把这样表示的16进制字符串，还原成一个字节
+            b[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
+        }
+        return b;
+    }
 }

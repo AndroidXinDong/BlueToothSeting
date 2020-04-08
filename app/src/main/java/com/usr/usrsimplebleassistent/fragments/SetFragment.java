9727d20179aaa3a244e2e19123a0a69ece32ae6a
AndroidXinDong <991189873@qq.com>
2020-04-08 16:57:14 +0800

    阶段保存 2020-4-8 16:57:07

--- app/src/main/java/com/usr/usrsimplebleassistent/fragments/SetFragment.java
+++ app/src/main/java/com/usr/usrsimplebleassistent/fragments/SetFragment.java
@@ -1,10 +1,15 @@
 package com.usr.usrsimplebleassistent.fragments;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.usr.usrsimplebleassistent.R;
@@ -19,6 +24,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * create
@@ -26,27 +32,80 @@ import butterknife.ButterKnife;
  * by xinDong
  **/
 public class SetFragment extends Fragment {
+    @BindView(R.id.rBtn_set_ziwai)
+    RadioButton rBtnSetZiwai;
+    @BindView(R.id.rBtn_set_kejian)
+    RadioButton rBtnSetKejian;
+    @BindView(R.id.rBtn_set_red)
+    RadioButton rBtnSetRed;
+    @BindView(R.id.rBtn_set_harderware)
+    RadioButton rBtnSetHarderware;
     @BindView(R.id.bgcanshu)
     TextView bgcanshu;
+    @BindView(R.id.tv_y_bg)
+    TextView tvYBg;
+    @BindView(R.id.tv_y_lmd)
+    TextView tvYLmd;
+    @BindView(R.id.tv_y_ldbd)
+    TextView tvYLdbd;
+    @BindView(R.id.tv_y_mdbd)
+    TextView tvYMdbd;
+    @BindView(R.id.et_b_bg)
+    EditText etBBg;
+    @BindView(R.id.et_b_lmd)
+    EditText etBLmd;
+    @BindView(R.id.et_b_ldbd)
+    EditText etBLdbd;
+    @BindView(R.id.et_b_mdbd)
+    EditText etBMdbd;
+    @BindView(R.id.btn_bg)
+    Button btnBg;
+    @BindView(R.id.btn_lmd)
+    Button btnLmd;
+    @BindView(R.id.btn_ldbd)
+    Button btnLdbd;
+    @BindView(R.id.btn_mdbd)
+    Button btnMdbd;
+    @BindView(R.id.ll_set)
+    LinearLayout llSet;
+    @BindView(R.id.cb_one)
+    CheckBox cbOne;
+    @BindView(R.id.cb_two)
+    CheckBox cbTwo;
+    @BindView(R.id.btn_jdqset)
+    Button btnJdqset;
+    @BindView(R.id.ll_jidianqi)
+    LinearLayout llJidianqi;
+    private boolean jdqOne = false;
+    private boolean jdqTwo = false;
     private String TAG = "Tag";
 
     public SetFragment() {
     }
 
-    public static SetFragment getInstance(){
-        SetFragment fragment = new SetFragment();
-        Bundle bundle = new Bundle();
-        fragment.setArguments(bundle);
-        return fragment;
-    }
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_set,container,false);
-        ButterKnife.bind(SetFragment.this,view);
+        View view = inflater.inflate(R.layout.fragment_set, container, false);
+        ButterKnife.bind(SetFragment.this, view);
         EventBus.getDefault().register(SetFragment.this);
+        initCb();
         return view;
+    }
+
+    private void initCb() {
+        cbOne.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+               jdqOne = isChecked;
+            }
+        });
+        cbTwo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                jdqTwo = isChecked;
+            }
+        });
     }
 
     @Override
@@ -55,8 +114,8 @@ public class SetFragment extends Fragment {
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onShowEventMessage(MessageEvent event){
-//        Log.i(TAG, "onShowEventMessage: "+event.getMessage());
+    public void onShowEventMessage(MessageEvent event) {
+
     }
 
     @Override
@@ -64,4 +123,41 @@ public class SetFragment extends Fragment {
         super.onDestroy();
         EventBus.getDefault().unregister(SetFragment.this);
     }
+
+    @OnClick({R.id.rBtn_set_ziwai, R.id.rBtn_set_kejian, R.id.rBtn_set_red, R.id.rBtn_set_harderware,
+            R.id.btn_bg, R.id.btn_lmd, R.id.btn_ldbd, R.id.btn_mdbd, R.id.btn_jdqset})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.rBtn_set_ziwai:
+                hideJDQ();
+                break;
+            case R.id.rBtn_set_kejian:
+                hideJDQ();
+                break;
+            case R.id.rBtn_set_red:
+                hideJDQ();
+                break;
+            case R.id.rBtn_set_harderware:
+                llSet.setVisibility(View.GONE);
+                llJidianqi.setVisibility(View.VISIBLE);
+                break;
+            case R.id.btn_bg:
+                break;
+            case R.id.btn_lmd:
+                break;
+            case R.id.btn_ldbd:
+                break;
+            case R.id.btn_mdbd:
+                break;
+            case R.id.btn_jdqset:
+
+                break;
+        }
+    }
+
+    private void hideJDQ() {
+        llSet.setVisibility(View.VISIBLE);
+        llJidianqi.setVisibility(View.GONE);
+    }
+
 }
